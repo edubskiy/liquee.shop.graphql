9bf183cffcf3cc405bcf48800365836ba8168b4f
Evgeniy <edubskiy@openwaygroup.com>
2020-01-21 00:24:26 +0300

    Fetching single collection page with GraphQL using CollectionPageContainer

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -24,9 +24,6 @@ const CollectionOverviewContainer = () => (
   <Query query={GET_COLLECTIONS}>
   {
     ({ loading, error, data }) => {
-      console.log(loading);
-      console.log(error);
-      console.log(data);
       if (loading) return <Spinner />
 
       return <CollectionOverview collections={data.collections} />  
