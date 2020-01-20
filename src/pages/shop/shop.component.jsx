3a37cf8f187eb5c3950a8b16cc21f91be5d67e77
Evgeniy <edubskiy@openwaygroup.com>
2020-01-21 00:05:51 +0300

    Fetching collections with GraphQL on collections overview page with the help of HOC CollectionsOverviewContainer component with has handles query, loading state and rendering collections in CollectionOverview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.container';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({ match }) => (
